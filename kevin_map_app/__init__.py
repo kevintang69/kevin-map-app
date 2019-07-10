eadb27043e146d125c368e8ddab0346e6bbcf363
unknown <kevintang699669@gmail.com>
2019-07-09 21:56:16 -0700

    Signed-off-by: unknown <kevintang699669@gmail.com>

--- kevin_map_app/__init__.py
+++ kevin_map_app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coords.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://svpgomvwckxfor:28b9c719417b8b9d407c0d2c15f82188bbb9607ffe528527446a1661b0837f24@ec2-50-16-197-244.compute-1.amazonaws.com:5432/d8hl3grg73l3mt'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bdkoiypkzsecqh:5738bdab0d6126cb8c73b4df5a44d136344025b8c4e88a5cc9ead0eb5a27b83a@ec2-174-129-227-80.compute-1.amazonaws.com:5432/drthp8eu9os3e'
 
 app.config['SECRET_KEY'] = '81b7d9a160566fc877bfd2a86d581cb4'
 
